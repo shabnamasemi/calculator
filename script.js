4359becdb73dfe1dbb7ce549778595e559c6fee6
shabnamasemi <shbix.asm@gmail.com>
2025-11-03 16:20:29 +0330

    Adding a function to perform mathematical operations

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const  display = document.getElementById("display");
 
 function appendToDisplay(input){
     display.value += input;
-    
 }
 
 function clearDisplay(){
-
+    display.value = "";
 }
 
 function calculate(){
-
+    try{
+            display.value == EvalError(display.value);
+    }
+    catch(eror){
+        display.valueb = "eror";
+    }
 }
